24e85c6db1c355a50c5be2fbfbe456b7b34373ec
Bas Kolthof <b.kolthof92@gmail.com>
2017-11-04 21:07:10 +0100

    The Code so far.

---
+++ vsproject/ShitpostTron5000/BasicCommands.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+namespace Blehgh
+{
+    class BasicCommands
+    {
+        private static Random _rand = new Random();
+
+        [Command("say")]
+        public async Task Say(CommandContext ctx)
+        {
+            await ctx.RespondAsync(ctx.RawArgumentString);
+            Console.WriteLine($"I had to say {ctx.RawArgumentString}");
+        }
+
+        [Command("roll")]
+        public async Task Roll(CommandContext ctx, int dice, int dicen)
+        {
+            StringBuilder response = new StringBuilder();
+            response.Append($"Rolling {dice} d{dicen}ns\n");
+            response.Append("Your rolls are:```\n");
+            for (int i = 1; i < dice + 1; i++)
+            {
+                response.Append($"{i}:{_rand.Next(0, dicen)}\t");
+                if (i % 10 == 0) response.Append($"\n");
+            }
+            response.Append("```");
+            await ctx.RespondAsync(response.ToString());
+            Console.WriteLine($"I had to roll dice.");
+        }
+
+
+        [Command("status")]
+        public async Task Status(CommandContext ctx)
+        {
+
+            StringBuilder response = new StringBuilder();
+            response.Append($"I have been online since {Program.Start}  (uptime:{DateTime.Now - Program.Start})\n");
+            response.Append($"My ping is {Program.Client.Ping}\n");
+            response.Append($"I am connected to {Program.Client.Guilds.Count} Servers\n");
+
+            response.Append($"I am aware of the following channels on this server: ```\n");
+            foreach (DiscordChannel chanl in ctx.Guild.Channels)
+            {
+                response.Append($"{chanl.Name}:{chanl.Id} NSFW:{(chanl.IsNSFW ? "YES" : "NO")}\n");
+            }
+            response.Append("```");
+
+
+
+
+              await ctx.RespondAsync(response.ToString());
+            Console.WriteLine($"I gave a status update.");
+        }
+    }
+}
---
+++ vsproject/ShitpostTron5000/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
+
+namespace Blehgh
+{
+    class Program
+    {
+        public static DiscordClient Client;
+        static CommandsNextModule _commands;
+        public static DateTime Start;
+
+
+        static void Main(string[] args)
+        {
+            Start = DateTime.Now;
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            while (true)
+            {
+               string msg = Console.ReadLine();
+                Client.SendMessageAsync( Client.GetChannelAsync(245227159445045249).GetAwaiter().GetResult(), msg);
+            }
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            Client = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "Mzc2NDE3OTE3ODc4Nzk2Mjg4.DN-PqA.nWQ6PTqdYzJpH14ZeUZ1s2_LGFI",
+                TokenType = TokenType.Bot
+            });
+
+             _commands = Client.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "!"
+            });
+            _commands.RegisterCommands<BasicCommands>();
+
+            Client.MessageCreated += async e =>
+            {
+                if(e.Channel.Name == "devtrons")
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+
+            Client.VoiceStateUpdated += async e =>
+            {
+                await Client.SendMessageAsync(Client.GetChannelAsync(245227159445045249).GetAwaiter().GetResult(), $"{e.User.Username} just joined {e.Channel.Name}");
+            };
+
+
+            
+
+            await Client.ConnectAsync();
+           
+        }
+    }
+}
+
+
