acff89c9ad0337b89de187c02edaee29b1ef1c5e
Carbohydratos <Carbohydratos@gmail.com>
2020-03-18 18:53:26 +0100

    better quotes

--- vsproject/ShitpostTron5000/CommandsModules/QuoteDB.cs
+++ vsproject/ShitpostTron5000/CommandsModules/QuoteDB.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
@@ -17,20 +18,109 @@ namespace ShitpostTron5000
         public async Task GetRandomQuote(CommandContext ctx)
         {
             ShitpostTronContext db = Program.GetDbContext();
-            if(!db.Quotes.Any())
+            if (!db.Quotes.Any())
                 return;
-            
 
-            var qoteNumber = new Random().Next(db.Quotes.Count());
-            var result = db.Quotes.Skip(qoteNumber).First();
 
-            await ctx.RespondAsync($">>> {result.QuoteText}");
+            var quoteNumber = new Random().Next(db.Quotes.Count());
+            await SayQuote(ctx, db, quoteNumber);
         }
-        
+
+        [Command("Quote")]
+        public async Task GetQuote(CommandContext ctx, int number)
+        {
+            ShitpostTronContext db = Program.GetDbContext();
+            if (!db.Quotes.Any())
+                return;
+
+            await SayQuote(ctx, db, number);
+        }
+
+        private static async Task SayQuote(CommandContext ctx, ShitpostTronContext db, int quoteNumber)
+        {
+            var result = db.Quotes.FirstOrDefault(x => x.Id == quoteNumber);
+            if (result == null)
+            {
+                ctx.RespondAsync($"Could not find quote #{quoteNumber}");
+            }
+
+            await ctx.RespondAsync($"{result.QuoteText.Replace("\n", "\n> ")} \n―{result.QuoteeName} \t#{quoteNumber}");
+        }
 
 
-    [Command("quote")]
-        public async Task QuoteFromUser(CommandContext ctx, DiscordMember qotee, int skip =0)
+        [Command("quoteManual")]
+        public async Task QuoteFromUser(CommandContext ctx, DiscordMember attribute, [RemainingText]string Content)
+        {
+            var inter = Program.Client.GetInteractivityModule();
+
+            var attribName = attribute.DisplayName;
+
+            Regex mention = new Regex("<@!(?<id>\\d+)>");
+
+            Content = mention.Replace(Content, MentionTron);
+
+            var message = await ctx.RespondAsync($"Want me to add this to the quote DB?\n> {Content.Replace("\n", "\n> ")} \n―{attribName}");
+
+            await message.CreateReactionAsync(DiscordEmoji.FromUnicode("✅"));
+            await message.CreateReactionAsync(DiscordEmoji.FromUnicode("❎"));
+
+            var choice = await inter.WaitForReactionAsync(x => x.Name.Contains("✅",
+                                                                   StringComparison.OrdinalIgnoreCase) ||
+                                                               x.Name.Contains("❎",
+                                                                   StringComparison.OrdinalIgnoreCase),
+                ctx.User);
+
+            try
+            {
+                await message.DeleteAllReactionsAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Warning("did not clean reactons", ex);
+            }
+
+
+            if (choice.Emoji.Name.Contains("✅", StringComparison.OrdinalIgnoreCase))
+            {
+                await message.ModifyAsync("Working.");
+                ShitpostTronContext db = Program.GetDbContext();
+
+                Quote q = new Quote
+                {
+                    DiscordSnowFlake = 0,
+                    QuoteText = Content,
+                    QuoteeName = attribName,
+                    QouteeDiscordSnoflake = attribute.Id,
+
+                };
+                db.Add(q);
+                await db.SaveChangesAsync();
+                await message.ModifyAsync("Done!");
+
+            }
+            else
+            {
+                await message.ModifyAsync("No good after all?");
+            }
+            await Task.Delay(TimeSpan.FromSeconds(6));
+            await message.DeleteAsync();
+        }
+
+        private string MentionTron(Match x)
+        {
+            try
+            {
+                //ugly asyc handling bleh.
+                return "@" + Program.Client.GetUserAsync(ulong.Parse(x.Groups["id"].ToString())).Result.Username;
+            }
+            catch
+            {
+                return "@" + "███████████";
+            }
+        }
+
+        [Command("quoteChat")]
+        public async Task QuoteChat(CommandContext ctx, DiscordMember qotee, int skip = 0)
         {
             var candidates = await ctx.Channel.GetMessagesAsync(50);
             var targetMsg = candidates.Where(x => x.Author == qotee)
@@ -38,19 +128,16 @@ namespace ShitpostTron5000
                 .FirstOrDefault();
             if (targetMsg == null)
             {
-                await ctx.RespondAsync($"I cant find a recent message by that user{(skip!=0?$" or I skipped it.":"")}.");
+                await ctx.RespondAsync($"I cant find a recent message by that user{(skip != 0 ? $" or I skipped it." : "")}.");
                 return;
             }
 
             var inter = Program.Client.GetInteractivityModule();
 
-            var message =  await ctx.RespondAsync($"Want me to add this to the quote DB?\n>>> {targetMsg.Content}");
+            var message = await ctx.RespondAsync($"Want me to add this to the quote DB?\n>>> {targetMsg.Content}");
 
             await message.CreateReactionAsync(DiscordEmoji.FromUnicode("✅"));
             await message.CreateReactionAsync(DiscordEmoji.FromUnicode("❎"));
-
-           
-
 
             var choice = await inter.WaitForReactionAsync(x => x.Name.Contains("✅",
                                                                    StringComparison.OrdinalIgnoreCase) ||
@@ -63,9 +150,9 @@ namespace ShitpostTron5000
             }
             catch (Exception ex)
             {
-                Log.Warning("did not clean reactons",ex);
+                Log.Warning("did not clean reactons", ex);
             }
-            
+
 
             if (choice.Emoji.Name.Contains("✅", StringComparison.OrdinalIgnoreCase))
             {
